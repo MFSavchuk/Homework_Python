3f5288abe7b59f318d1faef6794491e9c941bb60
Mikhail Savchuk <mr.savchuk@bk.ru>
2022-11-10 07:08:06 +0300

    005

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from district.central_street.house1 import room1 as s1h1room1
+from district.central_street.house1 import room2 as s1h1room2
+from district.central_street.house2 import room1 as s1h2room1
+from district.central_street.house2 import room2 as s1h2room2
+
+from district.soviet_street.house1 import room1 as s2h1room1
+from district.soviet_street.house1 import room2 as s2h1room2
+from district.soviet_street.house2 import room1 as s2h2room1
+from district.soviet_street.house2 import room2 as s2h2room2
+
+from room_1 import folks as folks_room1
+from room_2 import folks as folks_room2
+
+
+people_room1 = [*s1h1room1.folks, *s1h2room1.folks, *s2h1room1.folks, *s2h2room1.folks, *folks_room1]
+people_room2 = [*s1h1room2.folks, *s1h2room2.folks, *s2h1room2.folks, *s2h2room2.folks, *folks_room2]
+
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
 
-
+print('В комнате room_1 живут: ' + ', '.join(people_room1))
+print('В комнате room_2 живут: ' + ', '.join(people_room2))
--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from district.central_street.house1 import room1 as s1h1room1
+from district.central_street.house1 import room2 as s1h1room2
+from district.central_street.house2 import room1 as s1h2room1
+from district.central_street.house2 import room2 as s1h2room2
+
+from district.soviet_street.house1 import room1 as s2h1room1
+from district.soviet_street.house1 import room2 as s2h1room2
+from district.soviet_street.house2 import room1 as s2h2room1
+from district.soviet_street.house2 import room2 as s2h2room2
+
 # Составить список всех живущих на районе и Вывести на консоль через запятую
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+people_district = [*s1h1room1.folks, *s1h2room1.folks, *s2h1room1.folks, *s2h2room1.folks, *s1h1room2.folks, *s1h2room2.folks, *s2h1room2.folks, *s2h2room2.folks]
+
+print('На районе живут: ' + ', '.join(people_district))
+print(f'Населенеие составляет {len(people_district)} человек ')
+print('Населенеие составляет %d человек ' % (len(people_district)))
+
 
 
 
