e368476ea01c57e66da70ee697a6f0e4deed373e
MikhailSavchuk2 <mr.savchuk@bk.ru>
2022-12-08 18:44:39 +0300

    10 - задача 02 finish

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import random
+
 
 # День сурка
 #
@@ -17,8 +19,59 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
-ENLIGHTENMENT_CARMA_LEVEL = 777
+class IamGodError(Exception):
+    pass
 
-# TODO здесь ваш код
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+
+def one_day():
+    karma = random.randint(1, 7)
+    dice_1 = random.randint(1, 13)
+    if dice_1 == 6:
+        dice_2 = random.choice(['IamGod', 'Drunk', 'CarCrash', 'Gluttony', 'Depression', 'Suicide'])
+        if dice_2 == 'IamGod':
+            raise IamGodError('Я бог')
+        if dice_2 == 'Drunk':
+            raise DrunkError('Умер от перебуха')
+        if dice_2 == 'CarCrash':
+            raise CarCrashError('Разбился на авто')
+        if dice_2 == 'Gluttony':
+            raise GluttonyError('Объелся и умер')
+        if dice_2 == 'Depression':
+            raise DepressionError('Умер от дипресии')
+        if dice_2 == 'Suicide':
+            raise SuicideError('Сошерщил самоубийсвто')
+    return karma
+
+
+ENLIGHTENMENT_CARMA_LEVEL = 777
+karma = 0
+
+while karma < ENLIGHTENMENT_CARMA_LEVEL:
+    try:
+        karma += one_day()
+        print(karma)
+    except Exception as exc:
+        with open('karma.txt', 'a', encoding='utf8') as ff:
+            ff.write(f'Случилось - {exc}\n')
 
 # https://goo.gl/JnsDqu
