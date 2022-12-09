f3130ad1016b38b2e4d46595c64f91e3753a6dc0
MikhailSavchuk2 <mr.savchuk@bk.ru>
2022-12-09 11:55:27 +0300

    10 - задача 03 finish

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,40 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+with open('registrations_good.log', 'w', encoding='utf8') as ff_bad:
+    ff_bad.write('')
+
+with open('registrations_bad.log', 'w', encoding='utf8') as ff_bad:
+    ff_bad.write('')
+
+
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+def check_line(line):
+    if len(line) <= 1:
+        raise ValueError('Строка пустая')
+    name, email, age = line.split(' ')
+    if not name.isalpha():
+        raise NotNameError('Имя содержит не только буквы')
+    if '@' and '.' not in email:
+        raise NotEmailError('Почта не содержит @ или . ')
+    if int(age) > 100 or int(age) < 10:
+        raise ValueError('Возраст меньше 10 или больше 99')
+
+
+with open('registrations.txt', 'r', encoding='utf8') as ff:
+    for line in ff:
+        try:
+            check_line(line=line)
+            with open('registrations_good.log', 'a', encoding='utf8') as ff_good:
+                ff_good.write(line)
+
+        except (ValueError, NotNameError, NotEmailError) as exc:
+            with open('registrations_bad.log', 'a', encoding='utf8') as ff_bad:
+                ff_bad.write(f'{line[:-1]} | Ошибка - "{exc}"\n')
