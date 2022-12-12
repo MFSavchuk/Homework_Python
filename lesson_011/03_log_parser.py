7d7c65de9395b27ad09d443267da3393399b9eb9
Mikhail Savchuk <mr.savchuk@bk.ru>
2022-12-12 17:39:39 +0300

    11 - задача 03 finish

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -14,4 +14,27 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+def groupedEvents(file):
+    with open(file, 'r', encoding='utf8') as ff:
+        prev_time = None
+        noc_count = 0
+
+        for line in ff:
+            if 'NOK' in line:
+                time = line[1:17]
+                if time != prev_time:
+                    if prev_time is not None:
+                        yield prev_time, noc_count
+
+                    prev_time = time
+                    noc_count = 0
+
+                noc_count += 1
+
+        if noc_count > 0:
+            yield prev_time, noc_count
+
+
+for group_time, event_count in groupedEvents('events.txt'):
+    print(f'[{group_time}] {event_count}')
