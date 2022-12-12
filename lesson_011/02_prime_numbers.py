78d2ffbcc35b76d1bcd8ea63761f3868ad109fc6
Mikhail Savchuk <mr.savchuk@bk.ru>
2022-12-12 16:17:32 +0300

    11 - задача 02 finish

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -43,12 +43,13 @@ class PrimeNumbers:
         else:
             raise StopIteration()
 
+
 #
 # prime_number_iterator = PrimeNumbers(n=10000)
 # for number in prime_number_iterator:
 #     print(number)
 
-# TODO после подтверждения части 1 преподователем, можно делать
+
 # Часть 2
 # Теперь нужно создать генератор, который выдает последовательность простых чисел до n
 # Распечатать все простые числа до 10000 в столбик
@@ -60,14 +61,33 @@ def prime_numbers_generator(n):
         number += 1
         for prime in range(2, number):
             if number % prime == 0:
-               break
+                break
         else:
             yield number
-    # TODO здесь ваш код
 
 
-for number in prime_numbers_generator(n=10000):
-    print(number)
+def isLucky(num):
+    number = str(num)
+    if len(number) % 2:
+        middle = len(number) // 2
+        return True if sum(map(int, number[:middle])) == sum(map(int, number[-middle:])) else False
+    else:
+        return False
+
+
+def isPolyndrom(num):
+    if isLucky(num):
+        number = str(num)
+        middle = len(number) // 2
+        return True if number[:middle] == number[-middle:] else False
+    else:
+        return False
+
+
+for number in prime_numbers_generator(n=30000):
+    if isLucky(num=number) and isPolyndrom(num=number):
+        print(number, isLucky(num=number), isPolyndrom(num=number))
+
 
 # Часть 3
 # Написать несколько функций-фильтров, которые выдает True, если число:
