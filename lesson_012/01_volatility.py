acbc349e6dfed5f7ff5da086621e8b4c2197b60a
Mikhail Savchuk <mr.savchuk@bk.ru>
2022-12-19 16:24:06 +0300

    12 - задача 01 finish

--- lesson_012/01_volatility.py
+++ lesson_012/01_volatility.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
+from collections import defaultdict
+from python_snippets.utils import time_track
 
 
 # Описание предметной области:
@@ -73,4 +76,87 @@
 #     def run(self):
 #         <обработка данных>
 
-# TODO написать код в однопоточном/однопроцессорном стиле
+
+class AnalyzerFileTransactions:
+
+    def __init__(self, path):
+        self.path = path
+        self.tickers_volatility = defaultdict(lambda: float)
+        self.ticker = None
+        self.volatility = None
+
+    def run(self):
+        with open(self.path, 'r', encoding='utf8') as ff:
+            price_list = []
+
+            for line in ff:
+                line = line[:-1]
+                secid, tradetime, price, quantity = line.split(',')
+                if 'SECID,TRADETIME,PRICE,QUANTITY' in line:
+                    continue
+                if self.ticker is None:
+                    self.ticker = secid
+                price = float(price)
+                price_list.append(price)
+
+        mix_price = min(price_list)
+        max_price = max(price_list)
+        average_price = (max_price + mix_price) / 2
+        self.volatility = round(((max_price - mix_price) / average_price) * 100, 1)
+        print(f'Номер тикера - {self.ticker}, Минимальная цена - {mix_price}, Максимальная цена - {max_price},'
+              f'Средняя цена - {average_price}, Волатильность - {self.volatility}%')
+
+        return self.ticker, self.volatility
+
+
+def print_result(tickers_volatility):
+    tickers_zero_volatility = defaultdict(lambda: float)
+    tickers_not_zero_volatility = defaultdict(lambda: float)
+    tickers_max_volatility = None
+    tickers_min_volatility = None
+
+    for ticker, volatility in tickers_volatility.items():
+        if volatility == 0:
+            tickers_zero_volatility[ticker] = volatility
+        else:
+            tickers_not_zero_volatility[ticker] = volatility
+    tickers_max_volatility = sorted(tickers_not_zero_volatility.items(), key=lambda x: x[1], reverse=True)[:3]
+    tickers_min_volatility = sorted(tickers_not_zero_volatility.items(), key=lambda x: x[1], reverse=False)[:3]
+    tickers_zero_volatility = sorted(tickers_zero_volatility.items(), key=lambda x: x[0])
+    # print(tickers_zero_volatility)
+    # print(tickers_min_volatility)
+    # print(tickers_max_volatility)
+    print('\n')
+    print('{a:-^32}'.format(a='-'))
+    print('|{a:^30}|'.format(a='Максимальная волатильность:'))
+    print('|{a:-^30}|'.format(a='-'))
+    for ticker, volatility in tickers_max_volatility:
+        print('|{a:^30}|'.format(a=f'ТИКЕР-{ticker} - {volatility} %'))
+    print('|{a:-^30}|'.format(a='-'))
+    print('|{a:^30}|'.format(a='Минимальная волатильность:'))
+    print('|{a:-^30}|'.format(a='-'))
+    for ticker, volatility in tickers_min_volatility:
+        print('|{a:^30}|'.format(a=f'ТИКЕР-{ticker} - {volatility} %'))
+    print('{a:-^32}'.format(a='-'))
+    print('{a:^30}'.format(a='Нулевая волатильность:'))
+    print('\t', end='')
+    for ticker, volatility in tickers_zero_volatility:
+        print(f'ТИКЕР-{ticker},', end=' ')
+    print('\n')
+
+
+@time_track
+def my_func():
+    my_path = 'trades'
+    tickers = defaultdict(lambda: float)
+    for dirpath, dirnames, filenames in os.walk(my_path):
+        for file in filenames:
+            full_file_path = os.path.join(dirpath, file)
+            my_analyzer = AnalyzerFileTransactions(path=full_file_path)
+            ticker, volatility = my_analyzer.run()
+            tickers[ticker] = volatility
+
+    print_result(tickers_volatility=tickers)
+
+
+my_func()
